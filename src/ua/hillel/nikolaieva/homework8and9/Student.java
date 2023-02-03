f9ea9e56a85aaabf6c87396339b3f4c15bac9989
Olena Nikolaieva <nikolayeva.alyona@gmail.com>
2023-02-03 16:00:25 +0100

    not used code was removed

--- src/ua/hillel/nikolaieva/homework8and9/Student.java
+++ src/ua/hillel/nikolaieva/homework8and9/Student.java
@@ -13,15 +13,7 @@ public class Student {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCourse() {
         return course;
-    }
-
-    public void setCourse(int course) {
-        this.course = course;
     }
 }
