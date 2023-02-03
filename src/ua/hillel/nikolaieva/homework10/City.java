5ad46e0c6d41bf181bda5efc1a80ba6e3a0f6fc9
Olena Nikolaieva <nikolayeva.alyona@gmail.com>
2023-02-03 15:50:58 +0100

    added homework10

---
+++ src/ua/hillel/nikolaieva/homework10/City.java
@@ -0,0 +1,15 @@
+package ua.hillel.nikolaieva.homework10;
+
+public class City {
+    private String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
