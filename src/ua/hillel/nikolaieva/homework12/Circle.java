25e175dbd79d1dd5dc14bf6fd16105dc2c34ae8f
Olena Nikolaieva <nikolayeva.alyona@gmail.com>
2023-04-06 17:26:25 +0200

    added homework 12 - sorted array of shapes

---
+++ src/ua/hillel/nikolaieva/homework12/Circle.java
@@ -0,0 +1,19 @@
+package ua.hillel.nikolaieva.homework12;
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ src/ua/hillel/nikolaieva/homework12/Shape.java
@@ -0,0 +1,8 @@
+package ua.hillel.nikolaieva.homework12;
+
+abstract class Shape {
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+}
